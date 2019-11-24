b6bb83a567d734f738de7066cef8c9aae9cc7965
Seong Gi Dong <dirrksdl@naver.com>
2019-11-24 16:41:38 +0900

    Table 컴포넌트 위치이동

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route} from 'react-router-dom'
 import Main from './pages/Main'
-import Table from './pages/Table'
 import NoMatch from './pages/NoMatch'
 import SearchAppBar from './components/SearchAppBar'
 import SwipeableTemporaryDrawer from './components/SwipeableTemporaryDrawer'
@@ -15,9 +14,8 @@ class Router extends Component {
         return ( 
             <BrowserRouter>
                 <SearchAppBar/>
-                <Route path="/" exact component={Table}/>                    
-                <Route path="/Main" exact component={Main}/> 
-                <Route path="/Table" exact component={Table}/>      
+                <Route path="/" exact component={Main}/>                    
+                <Route path="/Main" exact component={Main}/>   
                 <Route path="/Left" component={SwipeableTemporaryDrawer}/>               
             </BrowserRouter>
 
