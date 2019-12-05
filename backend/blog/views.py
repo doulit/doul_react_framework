0a1e8632ac9914c2b788d3ad02a1144ff3ab3c64
Seong Gi Dong <dirrksdl@naver.com>
2019-12-05 22:03:18 +0900

    update 중

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -3,10 +3,15 @@ from django.core import serializers
 from django.http import HttpResponse,JsonResponse
 from backend.decorators import api_view
 from backend.utils import errors_message,success_message,execute,execute_and_serialize
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django import views
+import json
 from .serializers import BlogSerializer,MenuSerializer
 from rest_framework import generics
 from .form import Blog_Form,Menu_Form
 from .models import Blog,Menu
+from collections import OrderedDict
 
 # Create your views here.
 class MasterBlog(generics.ListCreateAPIView):
@@ -16,6 +21,43 @@ class MasterBlog(generics.ListCreateAPIView):
 class DetailBlog(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+
+
+class SaveBlog2(views.View):
+    @method_decorator(csrf_exempt)
+    # @api_view
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)        
+    def get(self, request):
+        return_message = OrderedDict()
+        return_message['code'] = -1
+        return_message['msg'] = "error"
+        
+        return json.dumps(return_message,ensure_ascii=False)
+    def post(self, request):
+        form = Blog_Form(request.JSON)
+        if request.JSON.get('id') != None:
+            post = get_object_or_404(Blog,pk=request.JSON['id'])
+            form = Blog_Form(request.JSON, instance=post)
+        elif form.is_valid():
+            blog = form.save(commit=False)
+            blog.save()
+            return success_message()      
+        else:
+            print(errors_message(form))
+            return errors_message(form)
+    def delete(self, request):
+        form = Blog_Form(request.JSON)
+        if request.JSON.get('id') != None:
+            post = get_object_or_404(Blog,pk=request.JSON['id'])
+            form = Blog_Form(request.JSON, instance=post)
+        elif form.is_valid():            
+            blog = form.delete(commit=False)
+            blog.delete()
+            return success_message()      
+        else:
+            print(errors_message(form))
+            return errors_message(form)     
 
 @api_view
 def SaveBlog(request):    
